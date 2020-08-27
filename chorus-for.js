d1689adf22c302f9336d80900dc9ea651854fa79
SaraKate51115 <sarakate51115@gmail.com>
2020-08-27 02:11:35 +0000

    final changes

--- chorus-for.js
+++ chorus-for.js
@@ -3,3 +3,14 @@ for (let repeat = 0; repeat < 10; repeat ++){
   console.log(chorus);
 }
 console.log('Until the sun comes up!');
+
+function addTen(x) {
+  return x + 10;
+}
+
+function divideByThree(y) {
+  return y / 3;
+}
+
+var result = addTen(2);
+console.log(divideByThree(result));
